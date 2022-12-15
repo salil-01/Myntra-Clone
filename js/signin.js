4decb83eb90e92166107bdc2a6f8349d29b4bc21
Salil Upadhyay <upsalil.01@gmail.com>
2022-12-15 22:38:05 +0530

    added sign in and login form

---
+++ js/signin.js
@@ -0,0 +1,32 @@
+let storedData = JSON.parse(localStorage.getItem("userdata"))||[];
+let form = document.getElementById("input-data");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  storedData.forEach((element) => {
+    if (
+      element.email == form.email.value &&
+      element.password == form.password.value
+    ) {
+      console.log(true)
+        Swal.fire({
+            icon: 'success',
+            title: 'Login Successful',
+            showConfirmButton: false,
+          })
+      homepage();
+    } else {
+      console.log(false)
+      Swal.fire({
+        icon: 'error',
+        title: 'Wrong Credentials',
+        showConfirmButton: false,
+      })
+    }
+  });
+});
+let homepage = () => {
+    let tID = setTimeout(function () {
+        window.location.href = "homepage.html";
+        window.clearTimeout(tID);		// clear time out.
+    }, 2000);
+}
